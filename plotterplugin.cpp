d6d0ad5b8cb114d254d76cfc337982a8b7e69669
SillyConn <sillyconn@gmail.com>
2015-01-19 20:08:31 +0100

    Added fileheaders in plotterplugin.*

--- plotterplugin.cpp
+++ plotterplugin.cpp
@@ -1,3 +1,23 @@
+/*****************************************************
+ *
+ * Customized Function Plotter Widget
+ * written in use of the Qt Framework
+ *
+ * plotterplugin.cpp
+ *
+ * Date:    10.01.2014
+ * Author:  Sven Sperner
+ * eMail:   cethss@gmail.com
+ *
+ * Original version comes from
+ * "C++ GUI Programming with Qt 4, 2nd Edition"
+ *  by Jasmin Blanchette & Mark Summerfield
+ *
+ * Copyright (c) All rights reserved
+ *
+ * ***************************************************/
+
+
 #include "plotter.h"
 #include "plotterplugin.h"
 
--- plotterplugin.h
+++ plotterplugin.h
@@ -1,3 +1,23 @@
+/*****************************************************
+ *
+ * Customized Function Plotter Widget
+ * written in use of the Qt Framework
+ *
+ * plotterplugin.h
+ *
+ * Date:    10.01.2014
+ * Author:  Sven Sperner
+ * eMail:   cethss@gmail.com
+ *
+ * Original version comes from
+ * "C++ GUI Programming with Qt 4, 2nd Edition"
+ *  by Jasmin Blanchette & Mark Summerfield
+ *
+ * Copyright (c) All rights reserved
+ *
+ * ***************************************************/
+
+
 #ifndef PLOTTERPLUGIN_H
 #define PLOTTERPLUGIN_H
 
